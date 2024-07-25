5bb87711f5dc3dca118bc54f572abe03dfd25838
ME20311762 <104609498+ME20311762@users.noreply.github.com>
2024-07-25 11:24:32 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+var selfstatus = "success!";
+console.log(selfstatus);
+
+var strDest = "sriram";
+
+if (strDest !== undefined && strDest != null )
+{
+    console.log("IF...");
+    console.log(strDest.length);
+    console.log(strDest.value);
+    console.log(strDest);
+}else{
+    console.log("else...");
+    console.log(strDest.length);
+    console.log(strDest);
+}
